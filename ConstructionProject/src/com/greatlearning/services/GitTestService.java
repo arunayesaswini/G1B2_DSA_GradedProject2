2ba054bfb69438296d84d9f7fc4a0497835422c6
Tejaswi <Tejaswi@DESKTOP-7OQBODM>
2023-06-27 15:06:18 +0530

    created GitTestService class

---
+++ ConstructionProject/src/com/greatlearning/services/GitTestService.java
@@ -0,0 +1,13 @@
+package com.greatlearning.services;
+
+public class GitTestService 
+{
+	public void display()
+	{
+		System.out.println("Entered into Git Services Class");
+		System.out.println("Services are:");
+		System.out.println("push, pull, commit, merge, rebase ");
+		
+	}
+
+}
